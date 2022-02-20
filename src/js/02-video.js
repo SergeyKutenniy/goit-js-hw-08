e0d9120312641208dffb6f8596ef8334720a68a5
Sergey Kutenniy <kutenniysergey@gmail.com>
2022-02-20 22:22:19 +0200

    task2-done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+// Воспроизвидение видео с того момента с которого остановилось (сохранение паузы в localStorage)
 
+const iframe = document.querySelector('iframe');
+
+const player = new Vimeo.Player(iframe); // инициализация плеера
+
+import throttle from 'lodash.throttle'; // отслеживание текущего времени воспроизвидения каждую 1с.
+
+const currentTime = 'videoplayer-current-time'; // переменная хранения текущего времени в localStorage
+
+const key = localStorage.getItem(currentTime); // сохраняем текущее время в localStorage
+
+if (key) {
+  player.setCurrentTime(key); // получаем значение текущего времени
+}
+
+player.on(
+  'timeupdate',
+  throttle(data => {
+    localStorage.setItem(currentTime, data.seconds.toString());
+  }, 1000),
+);
