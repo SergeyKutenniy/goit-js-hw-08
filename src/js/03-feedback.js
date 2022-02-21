a8c54820d22e914675dcb665f72a4a3a5d263599
Sergey Kutenniy <kutenniysergey@gmail.com>
2022-02-21 22:28:15 +0200

    task3-done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,42 @@
+import throttle from 'lodash.throttle';
 
+const key = 'feedback-form-state'; // ключ который хранит данные в localStorage
+const formData = {}; // пустой обьект который будет хранить данные
+
+const form = document.querySelector('.feedback-form'); // доступ к форме
+
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onFormInput, 500));
+
+updateForm();
+
+function updateForm() {
+  const saveData = localStorage.getItem(key);
+  if (saveData) {
+    const { email, message } = JSON.parse(saveData);
+    form.email.value = email;
+    form.message.value = message;
+    formData.email = email;
+    formData.message = message;
+  }
+}
+
+function onFormInput() {
+  formData.email = form.elements.email.value;
+  formData.message = form.elements.message.value;
+  localStorage.setItem(key, JSON.stringify(formData));
+}
+
+function onFormSubmit() {
+  event.preventDefault();
+
+  const formDataToSend = new FormData(event.currentTarget);
+  formDataToSend.forEach((value, name) => {
+    formData[name] = value;
+  });
+
+  event.currentTarget.reset();
+  localStorage.removeItem(key);
+
+  console.log(formData);
+}
